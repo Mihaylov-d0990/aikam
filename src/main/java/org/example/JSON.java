0596f45fbac8cf027a53a1dc1f39971b101dcf97
Danila Mihaylov <danila.mihaylov@office.e2e4gu.ru>
2022-08-30 21:38:14 +0700

    Implemented method for error handling

--- src/main/java/org/example/JSON.java
+++ src/main/java/org/example/JSON.java
@@ -15,6 +15,80 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class JSON {
+    private final HashSet<String> searchCriterias = new HashSet<String>(
+            Arrays.asList("lastName", "productName", "minTimes", "minExpenses", "maxExpenses", "badCustomers")
+    );
+    private final HashSet<String> numericCriterias = new HashSet<String>(
+            Arrays.asList("minTimes", "minExpenses", "maxExpenses", "badCustomers")
+    );
+
+
+    private String checkCriteriasStatFormat(JSONObject inputCriterias) {
+        if (inputCriterias == null) {
+            return "Не найдены входные критерии";
+        }
+
+        if (inputCriterias.get("startDate") == null) {
+            return "Отсутствует поле startDate";
+        }
+
+        if (inputCriterias.get("endDate") == null) {
+            return "Отсутствует поле endDate";
+        }
+
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setLenient(false);
+        try {
+            sdf.parse(inputCriterias.get("startDate").toString());
+            sdf.parse(inputCriterias.get("endDate").toString());
+        } catch (java.text.ParseException e) {
+            return "Неправильный формат даты";
+        }
+
+        return null;
+    }
+
+    private String checkCriteriasSearchFormat(JSONArray criterias) {
+        if (criterias == null) {
+            return "Невозможно найти поле criterias";
+        }
+
+        if (criterias.isEmpty()) {
+            return "Отсутствуют критерии поиска";
+        }
+
+        for (Object criteria: criterias) {
+            JSONObject jsonObject = (JSONObject) criteria;
+            if (jsonObject.get("productName") != null && jsonObject.get("minTimes") == null) {
+                return "Критерий productName найден. Критерий minTimes не найден";
+            }
+            if (jsonObject.get("productName") == null && jsonObject.get("minTimes") != null) {
+                return "Критерий productName не найден. Критерий minTimes найден";
+            }
+            if (jsonObject.get("minExpenses") != null && jsonObject.get("maxExpenses") == null) {
+                return "Критерий minExpenses найден. Критерий maxExpenses не найден";
+            }
+            if (jsonObject.get("minExpenses") == null && jsonObject.get("maxExpenses") != null) {
+                return "Критерий minExpenses не найден. Критерий maxExpenses найден";
+            }
+            for (Object criteriaName: jsonObject.keySet()) {
+                if (!searchCriterias.contains(criteriaName)) {
+                    return "Неизвестный критерий поиска: " + criteriaName;
+                }
+            }
+
+            for (String field : numericCriterias) {
+                try {
+                    if (jsonObject.get(field) != null) {
+                        Integer.parseInt(jsonObject.get(field).toString());
+                    }
+                } catch (NumberFormatException e) {
+                    return "Неправильный формат " + field;
+                }
+            }
+        }
+        return null;
+    }
 
     public void writeOutputJson(String outputFilePath, JSONObject result) {
         File file = new File(outputFilePath);
