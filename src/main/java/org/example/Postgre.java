83cfc2e8fbdcfba2150db6c6d7c0532b515688e6
Danila Mihaylov <danila.mihaylov@office.e2e4gu.ru>
2022-08-30 21:42:16 +0700

    Implemented method for fetching data from database with stat operation type

--- src/main/java/org/example/Postgre.java
+++ src/main/java/org/example/Postgre.java
@@ -39,6 +39,88 @@ public class Postgre {
         }
     }
 
+    public JSONObject getOutputForStat(JSONObject criterias) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            Statement statement = connection.createStatement();
+            JSONObject outputObject = new JSONObject();
+            ResultSet resultSet = statement.executeQuery("SELECT customer.firstname, customer.lastname, product.name AS product, \n" +
+                    "SUM(product.price) AS expenses, COUNT(*) AS totalDays FROM purchase \n" +
+                    "INNER JOIN customer ON customer.customerid = purchase.customer\n" +
+                    "INNER JOIN product ON product.productid = purchase.product\n" +
+                    "WHERE purchase.date BETWEEN '" + criterias.get("startDate") + "' AND '" + criterias.get("endDate") + "'\n" +
+                    "AND extract(dow from purchase.date) != 0 AND extract(dow from purchase.date) != 6\n" +
+                    "GROUP BY (customer.firstname, customer.lastname, product.name)");
+
+            Integer totalExpensesGlobal = 0;
+            Integer totalDaysGlobal = 0;
+
+            if (resultSet != null) {
+                HashMap<JSONObject, JSONArray> customers = new HashMap<JSONObject, JSONArray>();
+                JSONArray purchases = new JSONArray();
+                while (resultSet.next()) {
+                    JSONObject customer = new JSONObject();
+                    String firstName = resultSet.getString("firstName");
+                    String lastName = resultSet.getString("lastName");
+                    customer.put("name", firstName + " " + lastName);
+
+                    JSONObject productObject = new JSONObject();
+                    String product = resultSet.getString("product");
+                    Integer expenses = Integer.parseInt(resultSet.getString("expenses"));
+                    productObject.put("name", product);
+                    productObject.put("expenses", expenses);
+
+                    if (customers.containsKey(customer)) {
+                        purchases.add(productObject);
+                        customers.put(customer, purchases);
+                    } else {
+                        purchases = new JSONArray();
+                        purchases.add(productObject);
+                        customers.put(customer, purchases);
+                    }
+
+                    Integer totalDays = Integer.parseInt(resultSet.getString("totalDays"));
+                    totalDaysGlobal += totalDays;
+                    totalExpensesGlobal += expenses;
+                }
+
+                JSONArray customersArray = new JSONArray();
+
+                Integer countCustomers = 0;
+                for (Map.Entry<JSONObject, JSONArray> entry : customers.entrySet()) {
+                    JSONObject customer = new JSONObject();
+                    customer.put("name", entry.getKey().get("name"));
+                    JSONArray customerPurchases = new JSONArray();
+                    Integer totalExpensesByCustomer = 0;
+
+                    for (Object productObject : entry.getValue()) {
+                        JSONObject product = (JSONObject) productObject;
+                        customerPurchases.add(product);
+                        totalExpensesByCustomer += Integer.parseInt(product.get("expenses").toString());
+                    }
+
+                    customer.put("purchases", customerPurchases);
+                    customer.put("totalExpenses", totalExpensesByCustomer);
+                    customersArray.add(customer);
+                    countCustomers++;
+                }
+
+                outputObject.put("type", "stat");
+                outputObject.put("totalDays", totalDaysGlobal);
+                outputObject.put("customers", customersArray);
+                outputObject.put("totalExpenses", totalExpensesGlobal);
+                outputObject.put("avgExpenses", String.format("%.2f", (float) totalExpensesGlobal / countCustomers));
+            }
+            return outputObject;
+
+        } catch (SQLException e) {
+            System.err.println(e);
+            JSONObject errorObject = new JSONObject();
+            errorObject.put("type", "error");
+            errorObject.put("message", e.getMessage());
+            return errorObject;
+        }
+    }
+
     private JSONObject getCriteriaQueryResult(JSONObject criteria, Connection connection) {
         try (Statement statement = connection.createStatement()) {
             JSONObject criteriaResultObject = new JSONObject();
